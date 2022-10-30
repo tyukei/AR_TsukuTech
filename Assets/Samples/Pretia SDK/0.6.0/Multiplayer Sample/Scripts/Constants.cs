1d60534993909ddd9b899e76be24d048c04fff11
Keita Nakata <0024481294bxyk6037@gmail.com>
2022-10-30 16:27:59 +0900

    add: Samples

---
+++ Assets/Samples/Pretia SDK/0.6.0/Multiplayer Sample/Scripts/Constants.cs
@@ -0,0 +1,12 @@
+namespace PretiaArCloud.Samples.ShooterSample
+{
+    public static class Constants
+    {
+        public const string ACCESS_TOKEN_KEY = "ACCESS_TOKEN";
+        public const string MULTIPLAYER_SCENE = "Main";
+        public const string MENU_SCENE = "Title";
+        public const string MULTIPLAYER_SCENE1 = "Main1";
+        //public const string MULTIPLAYER_SCENE = "1.MultiplayerGameScene";
+        //public const string MENU_SCENE = "0.MultiplayerMenuScene";
+    }
+}
